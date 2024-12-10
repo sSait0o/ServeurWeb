5c056cbf2cd3db0185c0fbc47f5c7759bb0e9f34
imed92 <imed.benamor.pro@gmail.com>
2024-12-10 15:05:43 +0100

    ko

--- app/Controllers/CoreController.php
+++ app/Controllers/CoreController.php
@@ -17,25 +17,25 @@ class CoreController
     {
         $absoluteURL = $_SERVER['BASE_URI'];
         global $router;
-        $typeModel = new Type();
-        $types = $typeModel->findAll();
+        // $typeModel = new Type();
+        // $types = $typeModel->findAll();
 
-        // Ci dessous on va créer un tableau qui sera ue "copie" de $types (liste des types)
-        // Grace a ce tableau on pourra piocher facilement dans les types de produits
-        // Par ex, si on veut afficher le type 7 on afficher le $type[7]
-        // Pour faire simple dans ce tableau, les index seront = aux id
-        foreach($types as $elem) {
-            $typeListById[$elem->getId()] = $elem;
-        }
+        // // Ci dessous on va créer un tableau qui sera ue "copie" de $types (liste des types)
+        // // Grace a ce tableau on pourra piocher facilement dans les types de produits
+        // // Par ex, si on veut afficher le type 7 on afficher le $type[7]
+        // // Pour faire simple dans ce tableau, les index seront = aux id
+        // foreach($types as $elem) {
+        //     $typeListById[$elem->getId()] = $elem;
+        // }
         // dump($typeListById);
         // Ci dessous je créer une instance du model Category
         $categoryModel = new Category();
         // Ci dessous j'execute la methode findAll pour récupérer toutes les categories depuis la bdd
         $categories = $categoryModel->findAll();
         // dump($categories);
-        $brandModel = new Brand();
-        // Ci dessous j'execute la methode findAll pour récupérer toutes les categories depuis la bdd
-        $brands = $brandModel->findAll();
+        // $brandModel = new Brand();
+        // // Ci dessous j'execute la methode findAll pour récupérer toutes les categories depuis la bdd
+        // $brands = $brandModel->findAll();
 
         require_once __DIR__ . "/../views/partials/header.tpl.php";
         require_once __DIR__ . "/../views/$viewName.tpl.php";
